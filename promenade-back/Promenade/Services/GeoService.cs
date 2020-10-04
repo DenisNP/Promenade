64956118026e980b4bb6d4f6f03edff63172007e
Denis Peshekhonov <denis@peshekhonov.ru>
2020-10-04 10:31:30 +0300

    geo service start

---
+++ promenade-back/Promenade/Services/GeoService.cs
@@ -0,0 +1,33 @@
+﻿using Promenade.Models;
+using Promenade.Services.Abstract;
+
+namespace Promenade.Services
+{
+    public class GeoService
+    {
+        private readonly IDbService _dbService;
+        private readonly ContentService _contentService;
+
+        public GeoService(IDbService dbService, ContentService contentService)
+        {
+            _dbService = dbService;
+            _contentService = contentService;
+        }
+        
+        public User LoadUser(string userId)
+        {
+            var user = _dbService.ById<User>(userId);
+            if (user == null)
+            {
+                user = new User
+                {
+                    Id = userId,
+                    Categories = _contentService.GenerateInitial()
+                };
+                _dbService.UpdateAsync(user);
+            }
+
+            return user;
+        } 
+    }
+}
