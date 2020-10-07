468b63e7d17dd235bd9ecb9de002b2d791e7bba6
Denis Peshekhonov <denis@peshekhonov.ru>
2020-10-07 11:01:05 +0300

    social service fix

--- promenade-back/Promenade/Services/Abstract/ISocialService.cs
+++ promenade-back/Promenade/Services/Abstract/ISocialService.cs
@@ -4,8 +4,6 @@ namespace Promenade.Services.Abstract
 {
     public interface ISocialService
     {
-        void Notify(string[] userIds, string message, string hash = "");
-
         bool IsSignValid(string userId, Dictionary<string, string> pars, string sign);
     }
 }
--- promenade-back/Promenade/Services/VkService.cs
+++ promenade-back/Promenade/Services/VkService.cs
@@ -1,43 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Extensions.Logging;
 using Promenade.Services.Abstract;
 
 namespace Promenade.Services
 {
     public class VkService : ISocialService
     {
-        private readonly ILogger<VkService> _logger;
-        private const string VkApiAddress = "https://api.vk.com";
-        private const string VkApiVersion = "5.120";
-        private readonly string _vkApiKey;
         private readonly string _vkApiSecret;
 
-        public VkService(ILogger<VkService> logger)
+        public VkService()
         {
-            _logger = logger;
-            var key = Environment.GetEnvironmentVariable("PATH_TO_MASTERY_VK_TOKEN");
-            _vkApiKey = key ?? throw new ArgumentException("No VK Service token!");
-
-            var secret = Environment.GetEnvironmentVariable("PATH_TO_MASTERY_VK_SECRET");
+            var secret = Environment.GetEnvironmentVariable("PROMENADE_VK_SECRET");
+#if DEBUG
+            _vkApiSecret = "";
+#else
             _vkApiSecret = secret ?? throw new ArgumentException("No VK Secret key!");
-        }
-
-        public void Notify(string[] allUserIds, string message, string hash = "")
-        {
-            var uids = allUserIds.ToList();
-            while (uids.Count > 0)
-            {
-                var userIds = uids.Shift(100);
-                
-                MakeVkRequest("notifications.sendMessage", new Dictionary<string, string>
-                {
-                    {"user_ids", string.Join(",", userIds)},
-                    {"fragment", hash},
-                    {"message", message}
-                });
-            }
+#endif
         }
 
         public bool IsSignValid(string userId, Dictionary<string, string> pars, string sign)
@@ -52,12 +31,6 @@ namespace Promenade.Services
 
             var calculatedSign = Utils.ToBase64(Utils.HashHMAC(_vkApiSecret, parsString));
             return calculatedSign == sign && pars.ContainsKey("vk_user_id") && pars["vk_user_id"] == userId;
-        }
-
-        private string MakeVkRequest(string method, Dictionary<string, string> data)
-        {
-            var address = $"{VkApiAddress}/method/{method}?v={VkApiVersion}&access_token={_vkApiKey}";
-            return Utils.PostRequest(address, data, _logger);
         }
     }
 }
