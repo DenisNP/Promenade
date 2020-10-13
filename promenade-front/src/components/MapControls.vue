87aeba6062a80f1a4f46daa2c1ed1f44a739ef48
Denis Peshekhonov <denis@peshekhonov.ru>
2020-10-13 23:46:43 +0300

    try fix shadow 2

--- promenade-front/src/components/MapControls.vue
+++ promenade-front/src/components/MapControls.vue
@@ -133,8 +133,8 @@ export default {
     box-shadow: 0 4px 10px rgba(0, 0, 0, 0.25);
     border-radius: 50%;
     display: flex;
-    align-items: center; /*высота*/
-    justify-content: center; /*ширина*/
+    align-items: center;
+    justify-content: center;
     z-index: 1;
 }
 
@@ -151,8 +151,9 @@ export default {
     display: flex;
     align-items: center; /*высота*/
     justify-content: center; /*ширина*/
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
     z-index: 2;
+    overflow: visible;
 }
 
 </style>
